f37bc4cbfa585870ede08cca3a534172a1733af9
Bingqing(Angelina) Ma <angelina890308@gmail.com>
2021-03-08 13:08:04 -0800

    add server.js back

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
+
+const PORT = process.env.PORT || 3080;
+
+const app = express();
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+app.get("/exercise", (req, res) => {
+  //TODO get query string in get requst by req.query.id, see workout.js
+  // if (req.query.id) {
+  //   res.redirect("/");
+  // } else {
+  res.sendFile(path.join(__dirname, "public/exercise.html"));
+  // }
+});
+
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/stats.html"));
+});
+app.listen(PORT, () => {
+  console.log(`listening to ${PORT}`);
+});
